d4eede8e65178adb5fb486d9094b1f99729698fa
Katherine Davenia <katherinedavenia24@gmail.com>
2022-10-13 13:27:12 +0700

    remove unused code

--- src/lib/cookies.js
+++
@@ -1,7 +0,0 @@
-import Cookies from 'js-cookie';
-
-export const setCookies = (key, value, options = undefined) => Cookies.set(key, value, options);
-
-export const getCookies = (key) => Cookies.get(key);
-
-export const removeCookies = (key) => Cookies.remove(key);
